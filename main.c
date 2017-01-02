884d0faf03b9a5442cf61c796f186111f92f2b46
Dong Qihong <dongqihong@vip.qq.com>
2017-01-02 16:55:13 +0800

    复习

--- main.c
+++ main.c
@@ -3,17 +3,17 @@
 #include "util.h"
 
 int main() {
-    int A[] = {9, 9, 8, 7, 6, 5, 4, 3, 2, 0};
+    int A[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
     int N = sizeof(A) / sizeof(A[0]);
     PrintArray(A, N);
     // InsertionSort(A, N);
     // BubbleSort(A, N);
     // OptimizedBubbleSort(A, N);
     // SelectionSort(A, N);
-    ShellSort(A, N);
+    // ShellSort(A, N);
     // HeapSort(A, N);
     // MergeSort(A, N);
-    // QuickSort(A, N);
+     QuickSort(A, N);
     PrintArray(A, N);
     VerifyOrder(A, N);
 
